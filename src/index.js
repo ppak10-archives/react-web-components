2aaa9ef10c5e4a793f9d001d165b4e0b9f6062af
Peter Pak <ppak10@gmail.com>
2020-12-14 18:43:16 -0600

    Adds react to repository.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@
  * Entry file for repository content
  */
 
+// Node Modules
+import {render} from 'react-dom';
+
 // Styles
 import 'styles/main.css';
+
+render(<div>Hello World From React</div>, document.getElementById('root'));
